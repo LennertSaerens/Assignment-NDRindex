f403378c8a3c37e87b688d1202785974bcf9e052
LennertSaerens <lennert.saerens@gmail.com>
2023-07-26 13:54:49 +0200

    Added a basic experiment

---
+++ experiments_NDRindex.py
@@ -0,0 +1,37 @@
+# experiments.py
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from NDRindex import NDRindex
+
+
+def generate_data(n_samples=100, n_features=10, n_clusters=3):
+    # Generate a synthetic dataset with n_samples samples, n_features features, and n_clusters clusters
+    data, _ = make_blobs(n_samples=n_samples, n_features=n_features, centers=n_clusters)
+    return data
+
+
+def run_experiment():
+    # Generate the data
+    data = generate_data()
+
+    # Normalize the data
+    scaler = StandardScaler()
+    normalized_data = scaler.fit_transform(data)
+
+    # Define the normalization and dimensionality reduction methods
+    normalization_methods = [lambda x: x]  # no normalization
+    pca = PCA(n_components=2)
+    dimension_reduction_methods = [pca.fit_transform]  # PCA for dimensionality reduction
+
+    # Create an NDRindex object
+    ndr = NDRindex(normalization_methods, dimension_reduction_methods)
+
+    # Evaluate the data quality
+    best_methods = ndr.evaluate_data_quality(normalized_data)
+    print(f"Best methods: {best_methods}")
+
+
+# Run the experiment
+run_experiment()
