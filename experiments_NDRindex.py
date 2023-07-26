1e467ebcbaeba0b3f155852fbf5bcd57965203dd
LennertSaerens <lennert.saerens@gmail.com>
2023-07-26 19:50:34 +0200

    Removed unneeded nomalisation step in experiment

--- experiments_NDRindex.py
+++ experiments_NDRindex.py
@@ -6,19 +6,16 @@ import matplotlib.pyplot as plt
 from NDRindex import NDRindex
 
 
-def generate_data(n_samples=1000, n_features=2, n_clusters=3, cluster_std=0.5, center_box=(-10.0, 10.0)):
+def generate_data(n_samples=10_000, n_features=2, n_clusters=3, cluster_std=0.5, center_box=(-10.0, 10.0)):
     # Generate a synthetic dataset with n_samples samples, n_features features, and n_clusters clusters
-    data, _ = make_blobs(n_samples=n_samples, n_features=n_features, centers=n_clusters, cluster_std=cluster_std, center_box=center_box)
+    data, _ = make_blobs(n_samples=n_samples, n_features=n_features, centers=n_clusters, cluster_std=cluster_std,
+                         center_box=center_box)
     return data
 
 
 def run_experiment():
     # Generate the data
     data = generate_data()
-
-    # Normalize the data
-    scaler = StandardScaler()
-    normalized_data = scaler.fit_transform(data)
 
     # Define the normalization and dimensionality reduction methods
     normalization_methods = [lambda x: x]  # no normalization
@@ -29,7 +26,7 @@ def run_experiment():
     ndr = NDRindex(normalization_methods, dimension_reduction_methods)
 
     # Evaluate the data quality
-    best_methods, best_score = ndr.evaluate_data_quality(normalized_data, 100)
+    best_methods, best_score = ndr.evaluate_data_quality(data, 10)
     print(f"Best methods: {best_methods}")
     print(f"Best score (NDRindex): {best_score}")
 
@@ -38,6 +35,6 @@ def run_experiment():
     plt.title('Generated Data')
     plt.show()
 
+
 # Run the experiment
 run_experiment()
-
