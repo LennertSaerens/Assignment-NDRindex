d85a33c412eefadf7f56d074a23dbafe9a5de9d5
LennertSaerens <lennert.saerens@gmail.com>
2023-08-15 14:28:44 +0200

    Added visualization of experiments results

--- experiment2_NDRindex.py
+++ experiment2_NDRindex.py
@@ -1,7 +1,6 @@
-import numpy as np
-from sklearn.metrics import adjusted_rand_score
-from scipy.cluster.hierarchy import linkage, fcluster
 from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+import matplotlib.pyplot as plt
 
 from NDRindex import NDRindex
 from experimentsSetup_NDRindex import *
@@ -35,13 +34,37 @@ kmeans = KMeans(n_clusters=len(np.unique(true_labels)), n_init=10)
 
 
 def run_experiment(dataset, ground_truth, clustering_method):
+    ari_scores = []
+    method_combinations = []
+
     for normalization_method in normalization_methods:
         normalized_data = normalization_method(dataset)
         for dimension_reduction_method in dimension_reduction_methods:
             reduced_data = dimension_reduction_method(normalized_data)
             clustering_labels = clustering_method(reduced_data)
             ari = adjusted_rand_score(ground_truth, clustering_labels)
+            ari_scores.append(ari)
+            method_combinations.append((normalization_method.__name__, dimension_reduction_method.__name__))
             print(f"ARI for {normalization_method.__name__} and {dimension_reduction_method.__name__}: {ari}")
 
+    return ari_scores, method_combinations
 
-run_experiment(yan_dataset, true_labels, kmeans.fit_predict)
+
+ari_scores, method_combinations = run_experiment(yan_dataset, true_labels, kmeans.fit_predict)
+chosen_ari = ari_scores[method_combinations.index(('scale_normalization', 'pca_reduction'))]
+avg_ari = np.mean(ari_scores)
+median_ari = np.median(ari_scores)
+upper_quartile_ari = np.percentile(ari_scores, 75)
+max_ari = np.max(ari_scores)
+
+# Metrics to plot
+metrics = ['Chosen ARI', 'Average ARI', 'Median ARI', 'Upper Quartile ARI', 'Max ARI']
+values = [chosen_ari, avg_ari, median_ari, upper_quartile_ari, max_ari]
+
+plt.bar(metrics, values, color=['red', 'green', 'blue', 'orange', 'purple'])
+plt.ylabel('ARI Score')
+plt.title('Comparison of ARI Metrics')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
